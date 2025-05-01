c21527723798817fbe0be24473e34da299670792
Imam Jamdar <imamjamdar784@gmail.com>
2025-05-01 18:09:22 +0530

    Add requirements.txt

--- app.py
+++ app.py
@@ -563,8 +563,12 @@ def upload_files():
 #     app.run(debug=True)
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
 
 
 
